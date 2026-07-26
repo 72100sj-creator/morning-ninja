68d8003acea33793682c7909a27a8845d2f79db9
Seb <72100sj@gmail.com>
2026-07-26 08:24:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    (pour ne JAMAIS servir une vieille version quand on est en ligne),
    le cache ne sert que de secours hors connexion. */
 
-var CACHE_NAME = "morning-ninja-v3.3";
+var CACHE_NAME = "morning-ninja-v3.8";
 
 var PRECACHE = [
   "./",
